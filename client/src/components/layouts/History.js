d7f466a61682a0fe91548954b6ca36067fc30b8c
QuangTruong252 <18521572@gm.uit.edu.vn>
2021-05-21 09:24:07 +0700

    edit History.js

--- client/src/components/layouts/History.js
+++ client/src/components/layouts/History.js
@@ -20,37 +20,37 @@ const History = (props) => {
       <>
         {generals.map((general) => (
           <tr>
-            <td class="td-info">
-              <div class="tr-info">
+            <td className="td-info">
+              <div className="tr-info">
                 <p>{general.date}</p>
               </div>
             </td>
-            <td class="td-info">
-              <div class="tr-info">
+            <td className="td-info">
+              <div className="tr-info">
                 <p>{general.time}</p>
               </div>
             </td>
             <td
               style={{ display: props.history.isTemperature ? "none" : "revert" }}
-              class="td-info"
+              className="td-info"
             >
-              <div class="tr-info">
+              <div className="tr-info">
                 <p>{general.data.temperature.value}</p>
               </div>
             </td>
             <td
               style={{ display: props.history.isOil ? "none" : "revert" }}
-              class="td-info"
+              className="td-info"
             >
-              <div class="tr-info">
+              <div className="tr-info">
                 <p>{general.data.oil.value}</p>
               </div>
             </td>
             <td
               style={{ display: props.history.isCo2 ? "none" : "revert" }}
-              class="td-info"
+              className="td-info"
             >
-              <div class="tr-info">
+              <div className="tr-info">
                 <p>{general.data.co2.value}</p>
               </div>
             </td>
@@ -62,50 +62,50 @@ const History = (props) => {
 
   return (
     <>
-      <div className="history">
-        <div className="history-head-flex">
+      <div classNameName="history">
+        <div classNameName="history-head-flex">
           <h3>History</h3>
           {/* <a href="/generality/history">
             <small>
               See all the history
-              <span className="las la-arrow-right" />
+              <span classNameName="las la-arrow-right" />
             </small>
           </a> */}
         </div>
-        <div className="table-responsive">
+        <div classNameName="table-responsive">
           <table>
             <tbody>
               <tr>
-                <td className="td-head">
-                  <div className="tr-head"><p>Date</p></div>
+                <td classNameName="td-head">
+                  <div classNameName="tr-head"><p>Date</p></div>
                 </td>
-                <td className="td-head">
-                  <div className="tr-head"><p>Time</p></div>
+                <td classNameName="td-head">
+                  <div classNameName="tr-head"><p>Time</p></div>
                 </td>
 
                 <td
                   style={{
                     display: props.history.isTemperature ? "none" : "revert",
                   }}
-                  className="td-head"
+                  classNameName="td-head"
                 >
-                  <div className="tr-head">
+                  <div classNameName="tr-head">
                     <p>Temperature</p>
                   </div>
                 </td>
                 <td
                   style={{ display: props.history.isOil ? "none" : "revert" }}
-                  className="td-head"
+                  classNameName="td-head"
                 >
-                  <div className="tr-head">
+                  <div classNameName="tr-head">
                     <p>Oil Moiture</p>
                   </div>
                 </td>
                 <td
                   style={{ display: props.history.isCo2 ? "none" : "revert" }}
-                  className="td-head"
+                  classNameName="td-head"
                 >
-                  <div className="tr-head">
+                  <div classNameName="tr-head">
                     <p>CO2 Connectration</p>
                   </div>
                 </td>
