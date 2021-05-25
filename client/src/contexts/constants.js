5424700bc5570829249e6699e3e4c59bd9bb90db
QuangTruong252 <18521572@gm.uit.edu.vn>
2021-05-25 16:31:12 +0700

    change ip server

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
+    ? "http://3.0.92.151:5000/api"
     : "something";
 export const LOCAL_STORAGE_TOKEN_NAME = 'agriculture_dashboard'
